461c0287c5c0f857b7cb813b4ee0ad1e40ce8eb2
Igor Morais <igor.morais@hollowsoft.com>
2026-04-29 07:29:54 -0300

    add list pet request

--- app/src/http/pet/request.ts
+++ app/src/http/pet/request.ts
@@ -2,6 +2,12 @@ import { Gender } from '@/type/gender'
 import { Kind } from '@/type/kind'
 import { Size } from '@/type/size'
 
+export type ListPetRequest = {
+  readonly page: number
+  readonly amount: number
+  readonly organization?: string
+}
+
 export type CreatePetRequest = {
   readonly name: string
   readonly kind: Kind
