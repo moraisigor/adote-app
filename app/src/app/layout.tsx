f31af4deaf633726a207acfea60b96011033854c
Igor Morais <igor.morais@hollowsoft.com>
2026-05-26 12:55:33 -0300

    update root layout

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,19 +1,39 @@
-'use client'
-
 import './style.css'
 
 import { ReactNode } from 'react'
 
+import { Footer, Header } from '@/component'
+
 import { Font } from './font'
 import { Provider } from './provider'
+
+const main = []
+
+const social = []
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html
       lang='en'
-      class={Font.className}>
-      <body>
-        <Provider>{children}</Provider>
+      class={`h-full bg-white dark:bg-gray-950 scheme-light dark:scheme-dark ${Font.className}`}>
+      <body class='h-full flex flex-col'>
+        <Provider>
+          <Header
+            option={{
+              url: '/',
+              name: 'Your Company',
+              icon: {} as any,
+              sign: { sign: 'Log in', onSign: () => {} }
+            }}
+            navigation={[]}
+          />
+          <main class='flex-1'>{children}</main>
+          <Footer
+            main={[]}
+            social={[]}
+            message={''}
+          />
+        </Provider>
       </body>
     </html>
   )
