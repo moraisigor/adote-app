839894bae76c0afa26935a69d903a9e4f49616a7
Igor Morais <igor.morais@hollowsoft.com>
2026-06-01 14:52:41 -0300

    update header component

--- app/src/component/Header/header.tsx
+++ app/src/component/Header/header.tsx
@@ -1,14 +1,14 @@
-import { ComponentType, SVGProps, useState, type ComponentProps, type FunctionComponent } from 'react'
-
-import { Bars3Icon } from '@heroicons/react/24/outline'
+import { ReactNode, useState, type ComponentProps, type FunctionComponent } from 'react'
 
 import { Dialog } from './dialog'
+
+import { AvatarIcon } from '../AvatarIcon'
 
 type HeaderProps = {
   option: {
     url: string
     name: string
-    icon: ComponentType<SVGProps<SVGSVGElement>>
+    children: ReactNode
     sign: {
       sign: string
       onSign: () => void
@@ -18,15 +18,10 @@ type HeaderProps = {
     url: string
     name: string
   }[]
-} & ComponentProps<'div'>
+} & ComponentProps<'header'>
 
 export const Header: FunctionComponent<HeaderProps> = ({ option, navigation, ...props }: HeaderProps) => {
-  const {
-    url,
-    name,
-    icon: Icon,
-    sign: { sign, onSign }
-  } = option
+  const { url, name, children } = option
 
   const [open, setOpen] = useState<boolean>(false)
 
@@ -37,46 +32,21 @@ export const Header: FunctionComponent<HeaderProps> = ({ option, navigation, ...
       <nav
         class='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
         aria-label='global'>
-        <div class='flex lg:flex-1'>
+        <div class='flex'>
           <a
             class='-m-1.5 p-1.5'
             href={url}>
             <span class='sr-only'>{name}</span>
-            <Icon />
+            {children}
           </a>
         </div>
-        <div class='flex lg:hidden'>
-          <button
-            class='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-indigo-300 dark:text-indigo-200'
-            type='button'
+        <div class='flex'>
+          <div
+            class='h-10 -m-1 -p-1'
             onClick={() => setOpen(true)}>
             <span class='sr-only'>open menu</span>
-            <Bars3Icon
-              class='size-6'
-              aria-hidden='true'
-            />
-          </button>
-        </div>
-        <div class='hidden lg:flex lg:gap-x-12'>
-          {navigation.map((e) => {
-            const { url, name } = e
-
-            return (
-              <a
-                class='text-sm/6 font-semibold text-white'
-                key={name}
-                href={url}>
-                {name}
-              </a>
-            )
-          })}
-        </div>
-        <div class='hidden lg:flex lg:flex-1 lg:justify-end'>
-          <span
-            class='text-sm/6 font-semibold text-white'
-            onClick={onSign}>
-            {sign} <span aria-hidden='true'>&rarr;</span>
-          </span>
+            <AvatarIcon />
+          </div>
         </div>
       </nav>
       <Dialog
