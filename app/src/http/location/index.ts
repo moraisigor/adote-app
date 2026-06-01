0c09f1fe472c5b9cb560a63c99f6e5caef894657
Igor Morais <igor.morais@hollowsoft.com>
2026-06-01 17:19:11 -0300

    update location request

--- app/src/http/location/index.ts
+++ app/src/http/location/index.ts
@@ -4,9 +4,10 @@ import { Location } from '@/type/location'
 
 import { client } from '../http.client'
 
-export const useSearchLocation = (search: string) => {
+export const useSearchLocation = (search: string, enabled: boolean = false) => {
   return useQuery({
     queryFn: () => client.get<Location[]>('/location', { params: { search } }).then((r) => r.data),
-    queryKey: ['SearchLocation', search]
+    queryKey: ['SearchLocation', search],
+    enabled
   })
 }
